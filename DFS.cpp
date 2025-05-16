976b938fd7ab2a735df04174a7415745b4e0d568
whoami <128888679+jahangirsohag@users.noreply.github.com>
2025-05-17 02:54:36 +0600

    Add files via upload

--- DFS.cpp
+++ DFS.cpp
@@ -1,58 +1,56 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <set>
 #include <algorithm>
-
-#define ll long long
 
 using namespace std;
 
-const int N = 1e5 + 9;
+const int N = 1e5+10;
 
-int tree[N];
-
-vector <int> g[N];
+vector <vector <int>> g(N);
 bool vis[N];
 
-void dfs(int u)
-{
-    vis[u] = true;
-    for(auto v : g[u])
-    {
-        if(!vis[v])
-        {
-            dfs(v);
-        }
-    }
+void dfs(int vertex) {
+
+	/** Take action on vertex after entering
+	*   the vertex
+	**/
+	vis[vertex] = true;
+	cout << vertex << " ";
+	for(int child : g[vertex])
+	{
+
+		/** Take action on child before
+		 *  entering the child node
+		 **/
+		if(vis[child] == true) continue;
+		dfs(child);
+
+		/** Take action on child
+		 *  after exiting child node
+		 **/
+
+	}
+
+	/** Take action on vertex before
+	 *  exiting the vertex
+	 **/
+
 }
 
 signed main() {
+	int n, m;
+	cin >> n >> m;
 
-    int n, m;
-    cin >> n >> m;
 
-    while(m--)
-    {
-        int u, v;
-        cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-
-    vector <int> components;
-
-    for(int u = 1; u <= n; ++u)
-    {
-        if(!vis[u])
-        {
-            components.push_back(u);
-            dfs(u);
-        }
-    }
-
-    cout << components.size() - 1 << endl;
-    for(int i = 0; i < components.size() - 1; ++i)
-    {
-        cout << components[i] << " " << components[i + 1] << endl;
-    }
-    return 0;
+	for(int i = 0; i < m; ++i)
+	{
+		int a, b;
+		cin >> a >> b;
+		g[a].push_back(b);
+		g[b].push_back(a);
+	}
+	dfs(1);
+   	return 0;
 }
